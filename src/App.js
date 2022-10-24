442b939e609675dcce0def8740d841b1a9aa55e2
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2022-10-25 03:56:15 +0600

    clint site project create and tailwind css added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Theme, Button } from "react-daisyui";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <Theme dataTheme="dark">
+        <Button color="primary">Click me, dark!</Button>
+      </Theme>
+
+      <Theme dataTheme="light">
+        <Button color="primary">Click me, light!</Button>
+      </Theme>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFwBuOjTGHEBIj1r0SvRjfOuukAft9b9s",
+  authDomain: "meta-learn-5c198.firebaseapp.com",
+  projectId: "meta-learn-5c198",
+  storageBucket: "meta-learn-5c198.appspot.com",
+  messagingSenderId: "915160117371",
+  appId: "1:915160117371:web:7ae8224e4c6e0ce065b2de",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
